d84d962f5a9d5911140f5c5ea369e38a1d6b84c4
Captain <raoofpick@gmail.com>
2020-05-29 23:01:49 +0430

    edit image loading with js

--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
+    
     Title = models.CharField(max_length=40)
     Slug = models.SlugField(unique=True)
     Brief = models.TextField(max_length=100)
@@ -29,10 +30,17 @@ class Post(models.Model):
     PostTag = models.ForeignKey(Tag,on_delete=models.CASCADE)
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='app/static/img/post'  , null=True)
     Status=models.CharField(max_length=20,choices=Statuses,default='p')
+
+    
+
 
     class Meta:
         ordering = ['-date',]
+    
+
+
     def __str__(self):
         return (self.Title)
 
--- app/views.py
+++ app/views.py
@@ -8,14 +8,12 @@ import datetime
 
 def MainPage (request):
     post = Post.objects.all()
+    
     return render(request,"app_html/index.html",{'post':post})
 
 
 def PostPage(request,slug):
     PostBlog = Post.objects.all().filter(Slug = slug)
-    PostValue=PostBlog.values()
-
-    ReleaseTime = (str(PostValue[0]['date'].year) + '/'+str(PostValue[0]['date'].month)+'/'+str(PostValue[0]['date'].day))
+   
     
-    
-    return render(request , "app_html/post.html",{'Postblog':PostBlog,'release':ReleaseTime})
+    return render(request , "app_html/post.html",{'Postblog':PostBlog})
