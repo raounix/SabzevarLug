b8047ef29de0e3c49d4892fb7e8b3384ca0dff80
Captain <raoofpick@gmail.com>
2020-05-31 03:47:39 +0430

    add paginator for page

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
@@ -8,8 +9,13 @@ import datetime
 
 def MainPage (request):
     post = Post.objects.all()
-    
-    return render(request,"app_html/index.html",{'post':post})
+    paginator = Paginator(post, 2) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    number = paginator.num_pages
+    print(number)
+    return render(request,"app_html/index.html",{'post':page_obj,'number':number})
 
 
 def PostPage(request,slug):
