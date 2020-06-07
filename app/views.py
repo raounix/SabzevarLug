5875da1851c3bd5c78be092d93ee75b36be3224b
Captain <raoofpick@gmail.com>
2020-06-08 04:07:34 +0430

    add custom filter in app named spl.py and add base.html for load from base file

--- app/views.py
+++ app/views.py
@@ -5,21 +5,36 @@ from .models import Post
 # Create your views here.
 from django.core import serializers
 from .models import Post
+from django import template
 import datetime
+from os.path import splitext
 
 def MainPage (request):
     post = Post.objects.all()
-    paginator = Paginator(post, 2) # Show 25 contacts per page.
+    paginator = Paginator(post, 3) # Show 25 contacts per page.
 
     page_number = request.GET.get('page')
+  
+    
     page_obj = paginator.get_page(page_number)
     number = paginator.num_pages
-    print(number)
+    
+
     return render(request,"app_html/index.html",{'post':page_obj,'number':number})
 
 
 def PostPage(request,slug):
     PostBlog = Post.objects.all().filter(Slug = slug)
+
    
     
     return render(request , "app_html/post.html",{'Postblog':PostBlog})
+
+
+
+register = template.Library()
+
+
+@register.filter
+def noext(value):
+    return splitext(value)[0]
