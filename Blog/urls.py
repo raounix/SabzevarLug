87f6dd47da3f09096dc6fd8e23e7d588769f239b
Raouf Baghejari <r.baghjeri@chakavak.info>
2020-12-22 13:38:17 +0330

    add gitignore

--- Blog/urls.py
+++ Blog/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('app.urls')),
+    path('manage/',include('AdminDashboard.urls')),
     path('admin/', admin.site.urls),
+    
 ]
