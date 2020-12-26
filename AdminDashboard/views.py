6e8fbbd27f37c797acffb294797644fb34bdfcc0
Raouf Baghejari <r.baghjeri@chakavak.info>
2020-12-26 11:54:56 +0330

    edit post edit

--- AdminDashboard/views.py
+++ AdminDashboard/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from app.models import News
 
 
 def MainPage(request):
     return render(request,"Dashboard/post_editor.html")
 
 def Submit_Post_Change(request):
-    print(request.POST['text'])
+    text=request.POST['text']
+    title=request.POST['title']
+    slug=request.POST['slug']
+    news = News(Title=title,Slug=slug,Brief=text,MainText=text)
+    news.save()
     return HttpResponse("ok")
