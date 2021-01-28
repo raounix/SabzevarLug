c3160d1aa16737d575df85d4860034cca26c3dad
Captain <raoofpick@gmail.com>
2021-01-28 12:39:43 +0330

    edit dashboard modules

--- AdminDashboard/urls.py
+++ AdminDashboard/urls.py
@@ -3,6 +3,6 @@ from AdminDashboard import views
 
 urlpatterns=[
     path("",views.MainPage),
-    path("news_change/",views.News_Change),
-    path("events_change/",views.Events_Change),
+    path("news/add",views.News_Change),
+    path("events/add",views.Events_Change),
 ]
--- AdminDashboard/views.py
+++ AdminDashboard/views.py
@@ -5,7 +5,7 @@ from app.models import News,Author,Event
 
 
 def MainPage(request):
-    return render(request,"Dashboard/events_edit.html")
+    return render(request,"Dashboard/base.html")
 
 def News_Change(request):
     if(request.method == "POST"):
@@ -14,18 +14,24 @@ def News_Change(request):
         slug=request.POST['slug']
         authors=Author.objects.all().filter(Author="raouf")
         news = News(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
-        news.save()
-        return HttpResponse("ok")
+        try:
+            news.save()
+            return HttpResponse("ok")
+        except :
+            return HttpResponse("error")
     elif(request.method =="GET"):
-        return render(request,"Dashboard/news_edit.html")
+        return render(request,"Dashboard/news_add.html")
 def Events_Change(request):
     if(request.method == "POST"):
         text=request.POST['text']
         title=request.POST['title']
         slug=request.POST['slug']
         authors=Author.objects.all().filter(Author="raouf")
-        news = Event(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
-        news.save()
-        return HttpResponse("ok")
+        event = Event(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
+        try:
+            event.save()
+            return HttpResponse("ok")
+        except :
+            return HttpResponse("error")
     elif(request.method =="GET"):
-        return render(request,"Dashboard/events_edit.html")
+        return render(request,"Dashboard/events_add.html")
