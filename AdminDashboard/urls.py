075213148ba7e6811369abdd71470f00a29e036e
Captain <raoofpick@gmail.com>
2021-01-28 15:32:43 +0330

    add login and logout to project

--- AdminDashboard/urls.py
+++ AdminDashboard/urls.py
@@ -3,6 +3,8 @@ from AdminDashboard import views
 
 urlpatterns=[
     path("",views.MainPage),
-    path("news/add",views.News_Change),
-    path("events/add",views.Events_Change),
+    path("news/add/",views.News_Change),
+    path("events/add/",views.Events_Change),
+    path("login/",views.Login),
+    path("logout/",views.Logout)
 ]
--- AdminDashboard/views.py
+++ AdminDashboard/views.py
@@ -1,37 +1,89 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 from app.models import News,Author,Event
 
+from django.contrib.auth import authenticate, login,logout
 
 def MainPage(request):
-    return render(request,"Dashboard/base.html")
+    if(request.user.is_authenticated ==False):
+        return redirect("/manage/login")
+    else:
+        return render(request,"Dashboard/base.html")
 
 def News_Change(request):
-    if(request.method == "POST"):
-        text=request.POST['text']
-        title=request.POST['title']
-        slug=request.POST['slug']
-        authors=Author.objects.all().filter(Author="raouf")
-        news = News(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
-        try:
-            news.save()
-            return HttpResponse("ok")
-        except :
-            return HttpResponse("error")
-    elif(request.method =="GET"):
-        return render(request,"Dashboard/news_add.html")
+    if(request.user.is_authenticated ==False):
+        return redirect("/manage/login")
+    else:
+        if(request.method == "POST"):
+            text=request.POST['text']
+            title=request.POST['title']
+            slug=request.POST['slug']
+            authors=Author.objects.all().filter(Author="raouf")
+            news = News(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
+            try:
+                news.save()
+                return HttpResponse("ok")
+            except :
+                return HttpResponse("error")
+        elif(request.method =="GET"):
+            return render(request,"Dashboard/news_add.html")
 def Events_Change(request):
-    if(request.method == "POST"):
-        text=request.POST['text']
-        title=request.POST['title']
-        slug=request.POST['slug']
-        authors=Author.objects.all().filter(Author="raouf")
-        event = Event(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
-        try:
-            event.save()
+    if(request.user.is_authenticated==False):
+        return redirect("/manage/login")
+    else:
+
+        if(request.method == "POST"):
+            text=request.POST['text']
+            title=request.POST['title']
+            slug=request.POST['slug']
+            authors=Author.objects.all().filter(Author="raouf")
+            event = Event(Title=title,Slug=slug,Brief=text,MainText=text,author=authors[0])
+            try:
+                event.save()
+                return HttpResponse("ok")
+            except :
+                return HttpResponse("error")
+        elif(request.method =="GET"):
+            return render(request,"Dashboard/events_add.html")
+
+
+def Login(request):
+    if(request.method=="POST"):
+        if(request.user.is_authenticated ==False):
+            try:
+                username=request.POST['username']
+                password=request.POST['password']
+                
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect("/manage")
+            except:
+                
+                return HttpResponse("error")
+        else:
             return HttpResponse("ok")
-        except :
-            return HttpResponse("error")
-    elif(request.method =="GET"):
-        return render(request,"Dashboard/events_add.html")
+        
+    elif (request.method=="GET"):
+        if(request.user.is_authenticated ==False):
+            return render(request,"Dashboard/login.html")
+        else:
+            return redirect("/manage")
+        
+
+
+
+def Logout(request):
+    if(request.user.is_authenticated ):
+
+        if(request.method=="GET"):
+          
+                logout(request)
+
+                return redirect('/manage/login/')
+
+            # return render(request,"Dashboard_Templates/login.html",{"login_status":"not_login"})
+        else:
+            return redirect('/manage/login/')
+    else:
+        return redirect('/manage/login/')
